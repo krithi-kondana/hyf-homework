b238e155667a8397a5778182d0b1b9df59ec7931
Krithi Kondana <55954596+krithi-kondana@users.noreply.github.com>
2020-01-25 18:24:16 +0100

    added javascript3 week1 homework

--- javascript/javascript3/week1/weatherapp/weatherapp.js
+++ javascript/javascript3/week1/weatherapp/weatherapp.js
@@ -1,14 +1,20 @@
-
+let errorMessage = document.getElementById('error')
 function fetchWeather() {
-  let placeValue = document.getElementById('place').value;
+  errorMessage.innerHTML = '';
+  const placeValue = document.getElementById('place').value;
+  if (placeValue === undefined || placeValue === '') {
+    errorMessage.innerHTML = 'Please enter city name and then click get weather!'
+  }
+  else {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${placeValue}&appid=84b5c59049054a6076ceb266293c7bc7`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        renderWeather(data);
+        renderMaps(data)
+      })
+  }
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${placeValue}&appid=84b5c59049054a6076ceb266293c7bc7`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      renderWeather(data);
-      renderMaps(data)
-    })
 }
 
 let weather = document.getElementById('btn')
@@ -16,33 +22,31 @@ weather.addEventListener('click', fetchWeather)
 
 
 
-
 function renderWeather(data) {
+
   document.getElementById('name').innerHTML = `<li>${data.name}</li>`
   document.getElementById('weather-description').innerHTML = `${data.weather[0].description}`
   document.getElementById('icon').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-
   document.getElementById('temperature').innerHTML = `${(data.main.temp - 273.15).toFixed(0)}°C`
   document.getElementById('list').innerHTML = `<li>Wind: ${data.wind.speed}m/s</li>
-                                                 <li>Clouds: ${data.clouds.all}%</li>
-                                                 <li>Sunrise: ${new Date(data.sys.sunrise * 1000).toLocaleTimeString()}</li>
-                                                 <li>Sunset: ${new Date(data.sys.sunset * 1000).toLocaleTimeString()}</li>                                                
-                                                 `
+                                               <li>Clouds: ${data.clouds.all}%</li>
+                                               <li>Sunrise: ${new Date(data.sys.sunrise * 1000).toLocaleTimeString()}</li>
+                                               <li>Sunset: ${new Date(data.sys.sunset * 1000).toLocaleTimeString()}</li>                                                
+                                              `
 }
 
 let map;
-let marker;
 function renderMaps(data) {
   let coordinates = {
     lat: data.coord.lat,
     lng: data.coord.lon
-  }  //Uncaught (in promise) TypeError: Cannot read property 'coord' of undefined (I dont know how to resolve this.Please help)
+  }
   map = new google.maps.Map(document.getElementById('map'), {
     center: coordinates,
     zoom: 10
   });
   // came up with a small extra feature(adding marker)
-  marker = new google.maps.Marker({ position: coordinates, map: map });
+  const marker = new google.maps.Marker({ position: coordinates, map: map });
 
 }
 
@@ -55,6 +59,7 @@ const location1 = document.getElementById('location')
 location1.addEventListener('click', myLocation);
 
 function fetchCurrentPosition(position) {
+  errorMessage.innerHTML = '';
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
   fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=84b5c59049054a6076ceb266293c7bc7`)
