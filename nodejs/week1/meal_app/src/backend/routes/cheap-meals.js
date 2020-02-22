820771bead0b8e69e5c3edbcd9bb56f38f414659
Krithi Kondana <55954596+krithi-kondana@users.noreply.github.com>
2020-02-22 13:04:02 +0100

    implemented feedback

--- nodejs/week1/meal_app/src/backend/routes/cheap-meals.js
+++ nodejs/week1/meal_app/src/backend/routes/cheap-meals.js
@@ -4,15 +4,10 @@ const app = express();
 const mealsJson = require(__dirname + '/../data/meals.json');
 const reviewsJson = require(__dirname + '/../data/reviews.json');
 
-const cheapMealsArray = mealsJson.filter(meal => meal.price < 50)
+const cheapMealsArray = mealsJson.filter(meal => meal.price < 50);
 cheapMealsArray.forEach(meal => {
-    meal.review = [];
-    reviewsJson.forEach(mealreview => {
-        if (mealreview.mealId === meal.id) {
-            meal.review.push(mealreview)
-        }
-    })
-})
+meal.review = reviewsJson.filter(mealReview => mealReview.mealId === meal.id)   
+});
 
 app.get("/", (request, response) => {
     response.send(cheapMealsArray);
