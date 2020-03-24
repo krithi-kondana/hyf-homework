7e422c9a9ec2fae4aebea2ced30457d2b982ce44
Krithi Kondana <55954596+krithi-kondana@users.noreply.github.com>
2020-03-24 12:34:10 +0100

    implemented feedback

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -5,12 +5,24 @@ app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
 
 // Calculator using :param
-app.get('/calculator/multiply', (req, res) => {
+app.get('/calculator/:method', (req, res) => {
         const arrayOfStringedNumbers = Object.values(req.query);
         const merged = [].concat.apply([], arrayOfStringedNumbers); //this is done to merge nested array caused by using same Params twice. Ex: [ '2', [ '2', '4' ] ]
         const arrayOfNumbers = merged.map(Number); // to unstring number
-        const productOfNumbers = arrayOfNumbers.reduce((total, num) => total * num)
-        res.send(`${productOfNumbers}`);
+        
+        if(req.params.method === 'multiply') {
+            const productOfNumbers = arrayOfNumbers.reduce((total, num) => total * num)
+            res.send(`${productOfNumbers}`);       
+        } else if(req.params.method === 'add') {
+            const sumOfNumbers = arrayOfNumbers.reduce((total, num) => total + num)
+            res.send(`${sumOfNumbers}`);           
+        } else if (req.params.method === 'divide') {
+            const divisionOfNumbers = arrayOfNumbers.reduce((total, num) => total / num)
+            res.send(`${divisionOfNumbers}`);
+        } else if (req.params.method === 'subtract') {
+            const subtractionOfNumbers = arrayOfNumbers.reduce((total, num) => total - num)
+            res.send(`${subtractionOfNumbers}`);
+            }
     })
     // http://localhost:3000/calculator/multiply?firstParam=1&secondParam=2&secondParam=4
 
